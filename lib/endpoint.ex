87cd6a0ab358bf9b6673fcf7562e4fd90b235b94
Giacomo Montibeller <giacomo.montibeller@xpeppers.com>
2020-03-09 16:21:08 +0100

    implement endpoints

--- lib/endpoint.ex
+++ lib/endpoint.ex
@@ -9,4 +9,30 @@ defmodule WebhookProcessor.Endpoint do
   get "/ping" do
     send_resp(conn, 200, "pong!")
   end
+
+  post "/events" do
+    {status, body} =
+      case conn.body_params do
+        %{"events" => events} -> {200, process(events)}
+        _ -> {422, missing_events()}
+      end
+
+    send_resp(conn, status, body)
+  end
+
+  defp process(events) when is_list(events) do
+    Poison.encode!(%{response: "Received events!"})
+  end
+
+  defp process(_) do
+    Poison.encode!(%{response: "Please send some events"})
+  end
+
+  defp missing_events do
+    Poison.encode!(%{error: "Expected Payload: { 'events': [...] }"})
+  end
+
+  match _ do
+    send_resp(conn, 404, "oops... Nothing here :(")
+  end
 end
