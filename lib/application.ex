de1206b0282750bd7a353653dfeab73d07e61e07
Giacomo Montibeller <giacomo.montibeller@xpeppers.com>
2020-03-10 12:28:56 +0100

    configure https

--- lib/application.ex
+++ lib/application.ex
@@ -4,9 +4,14 @@ defmodule WebhookProcessor.Application do
   def start(_type, _args) do
     children = [
       Plug.Cowboy.child_spec(
-        scheme: :http,
+        scheme: :https,
         plug: WebhookProcessor.Endpoint,
-        options: [port: Application.get_env(:webhook_processor, :port)]
+        options: [
+          port: Application.get_env(:webhook_processor, :port),
+          keyfile: "priv/keys/local.key",
+          certfile: "priv/keys/local.cert",
+          otp_app: :webhook_processor
+        ]
       )
     ]
 
